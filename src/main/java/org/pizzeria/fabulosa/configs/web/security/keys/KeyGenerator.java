93e4a6bc9c4b45688592daf9193fd744f7644047
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2025-01-12 20:33:20 +0100

    logging rsa keys

--- src/main/java/org/pizzeria/fabulosa/configs/web/security/keys/KeyGenerator.java
+++ src/main/java/org/pizzeria/fabulosa/configs/web/security/keys/KeyGenerator.java
@@ -1,8 +1,12 @@
 package org.pizzeria.fabulosa.configs.web.security.keys;
 
+import lombok.extern.slf4j.Slf4j;
+import org.pizzeria.fabulosa.utils.ExceptionLogger;
+
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
+@Slf4j
 public class KeyGenerator {
 	public static KeyPair generateRsaKeyPair() {
 		KeyPair keyPair;
@@ -11,7 +15,9 @@ public class KeyGenerator {
 			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 			keyPairGenerator.initialize(2048);
 			keyPair = keyPairGenerator.generateKeyPair();
+			log.info("RSA Public Key {}", keyPair.getPublic());
 		} catch (Exception e) {
+			ExceptionLogger.log(e, log, null);
 			throw new IllegalStateException();
 		}
 		return keyPair;
